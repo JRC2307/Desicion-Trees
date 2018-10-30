70d04f1aef554b18d31958687697c3e67b4a7283
estebanquintana6 <a01184855@itesm.mx>
2018-10-29 18:42:45 -0600

    Build tree from input

--- id3.py
+++ id3.py
@@ -3,27 +3,6 @@
 
 # Esteban Quintana
 # Javier Rodríguez
-import re
-import fileinput
-
-# Node def
-# Node Parser
-def parser():
-    attributes = []
-    atrr = []
-    val_list = []
-    for read in fileinput.input():
-        if "@attribute" in read:
-            attributes = re.split("[ \t]+|[ \t]+$", read,2)
-            attributes[2] = attributes[2].replace('{', "").replace('}', "").replace(' ', "").replace("\n", "")
-            attr = append.attributes[1]
-            val = append.attributes[2].split(",")
-            for v in val:
-                val_list.append(v)
-
-
-
-    return
 # Tree
 
 #ID3
@@ -34,3 +13,77 @@ def parser():
 
 ## Main
 #Read file
+
+import re
+import fileinput
+
+
+class Node:
+    def __init__(self, name, entropy, children, father, number, data_frame, answer, column):
+        self.entropy = entropy
+        self.name = name
+        self.children = children
+        self.father = father
+        self.number = number
+        self.data_frame = data_frame
+        self.answer = answer
+        self.column = column
+
+    def set_entropy(self, ent):
+        self.entropy = ent
+
+    def set_children(self, c):
+        self.children.append(c)
+
+    def set_father(self, f):
+        self.father = f
+
+    def show_tree(self, spaces):
+        if self.name != self.data_frame[0][-1]:
+            print("  " * spaces + self.column + ": " +self.name)
+        if self.answer:
+            print ("  " * spaces + "  " +  "ANSWER: " + self.answer)
+        for child in self.children:
+            child.show_tree(spaces + 1)
+
+
+def greater_gain(gains):
+    current_greater = 0.0
+    for g in gains:
+        if gains[g] > current_greater:
+            current_greater = gains[g]
+    for g in gains:
+        if gains[g] == current_greater:
+            return g
+
+
+def build_tree(data_frame, data):
+    root = Node(data_frame[0][-1], None, [], None, len(data_frame[0]) -1, data_frame, None, data_frame[0][-1])
+
+
+if __name__ == "__main__":
+    data_frame = []
+    attributes = []
+    data_types = {}
+    data = []
+    data_lines = False
+
+    for line in fileinput.input():
+        if "@attribute" in line:
+            str = re.split(' ', line, 2)
+            str.pop(0)
+            attributes.append(str[0])
+            str[1] = str[1].replace('\n', "").replace('{', "").replace('}', "").replace(' ', "")
+            values = str[1].split(',')
+            data_types[str[0]] = values
+        if data_lines:
+            aux_line = line.replace("\n", "")
+            values = aux_line.split(',')
+            data.append(values)
+        if "@data" in line:
+            data_lines = True
+    data_frame.append(attributes)
+    for d in data:
+        data_frame.append(d)
+    print(data_frame)
+    build_tree(data_frame, data_types)
